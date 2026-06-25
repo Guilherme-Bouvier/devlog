591a7d5ba315ca88430eaae340ab806cf3f096e1
Guilherme-Bouvier <guilhermebouva@gmail.com>
2026-06-25 20:41:07 -0300

    finalização aula

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from. import views                # Oponto (.) significa diretorio atual
+
+#Criamos a lidta de rotas DESTE aplicativo
+urlpatterns = [
+    #path ('caminho', função_da_view, nome_apelido)
+    #caminho vazio '' representa a página raiz
+    path('', views.home, name='home'),
+    path('sobre/', views.sobre_nos, name='sobre')
+
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#Importamos a ferramenta para devolver texto simples
+from django.http import HttpResponse
 
-# Create your views here.
+#Criamos a nossa função para a página inicial
+def home(request):
+
+    #O 'request' é o pedido (comando) do usuário.
+    mensagem = "<h1>Bem-vindo ao DevBlog!</h1> <p>Em breve, artigos aqui.</p>"
+
+    #Devcolvemos a resposta (o prato pronto)
+    return HttpResponse(mensagem)
+
+def sobre_nos(request):
+
+    #O 'request' é o pedido (comando) do usuário.
+    mensagem = "<h1>Sobre o DevBlog!</h1>"
+
+    #Devcolvemos a resposta (o prato pronto)
+    return HttpResponse(mensagem)
