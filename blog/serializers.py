4f1e8f7b2df874feb1237f9ec11d39ef52af0638
Guilherme-Bouvier <guilhermebouva@gmail.com>
2026-07-10 19:25:49 -0300

    v1 10/7

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from .models import Artigo, Categoria
 
+
 class ArtigoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artigo
-        fields = ['id', 'autor', 'titulo', 'categoria', 'conteudo', 'data_publicacao']
+        fields = [
+            'id',
+            'autor',
+            'titulo',
+            'categoria',
+            'conteudo',
+            'imagem',
+            'data_publicacao'
+        ]
+
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
